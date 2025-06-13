618f97f88d806e0479a86b9d508f7232009448c5
cutiefunny <cutiefunny@gmail.com>
2025-06-13 11:13:53 +0900

    pwa 구성

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWA from "@ducanh2912/next-pwa";
 
-export default nextConfig;
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/**', // 모든 경로의 이미지를 허용
+      },
+    ],
+  }
+};
+
+export default withPWA({
+  dest: "public",
+})(nextConfig);
