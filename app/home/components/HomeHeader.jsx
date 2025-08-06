a4c4b5a8fe62a8ab43e60eaa3061db51cdc8482a
cutiefunny <cutiefunny@gmail.com>
2025-08-07 01:46:04 +0900

    테마 색상 변경

--- app/home/components/HomeHeader.jsx
+++ app/home/components/HomeHeader.jsx
@@ -14,7 +14,7 @@ const SearchIcon = ({ isActive }) => (
     fill="none" 
     viewBox="0 0 24 24" 
     strokeWidth={1.5} 
-    stroke={isActive ? '#f5a623' : 'currentColor'} 
+    stroke={isActive ? '#FBC926' : 'currentColor'} 
     className={styles.icon}
   >
     <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
